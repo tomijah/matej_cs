e5c876044f3deb4cbaa0c49cb106894861200cc8
Tomasz Jaholkowski <jahol@ais.pl>
2016-09-29 04:59:37 +0200

    C

---
+++ C/WordCounter/WordCounter.cs
@@ -0,0 +1,60 @@
+﻿namespace WordCounter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class WordCounter
+    {
+        public const string WordSeparators = " ,.:[]()<>";
+
+        public static int Count(StreamReader text, string word)
+        {
+            int count = 0;
+            while (!text.EndOfStream)
+            {
+                count +=
+                    text.ReadLine()?
+                        .Split(WordSeparators.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                        .Count(x => x.Equals(word, StringComparison.InvariantCultureIgnoreCase)) ?? 0;
+            }
+
+            return count;
+        }
+
+        public static int AllWordsCount(StreamReader text)
+        {
+            int count = 0;
+
+            while (!text.EndOfStream)
+            {
+                count += text.ReadLine()?.Split(WordSeparators.ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Length ?? 0;
+            }
+
+            return count;
+        }
+
+        public static void AnalyzeFile(StreamReader text, Dictionary<string, uint> allWords)
+        {
+            while (!text.EndOfStream)
+            {
+                var words = text.ReadLine()?
+                    .Split(WordSeparators.ToCharArray(), StringSplitOptions.RemoveEmptyEntries) ?? new string[] { };
+
+                foreach (var word in words)
+                {
+                    if (allWords.ContainsKey(word))
+                    {
+                        allWords[word]++;
+                    }
+                    else
+                    {
+                        allWords.Add(word, 1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C/WordCounter/WordCounterTests.cs
@@ -0,0 +1,84 @@
+﻿namespace WordCounter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Text;
+
+    using FluentAssertions;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class WordCounterTests
+    {
+        private StreamReader PrepareInput(string input)
+        {
+            return new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(input)));
+        }
+
+        [Test]
+        public void CountMultipleLines()
+        {
+            var count = WordCounter.Count(PrepareInput(@"Test test test
+tests test:tttt"), "test");
+            count.Should().Be(4);
+        }
+
+        [Test]
+        public void CountOneLiner()
+        {
+            var count = WordCounter.Count(PrepareInput("Test test test"), "test");
+            count.Should().Be(3);
+        }
+
+        [Test]
+        public void SrtExample()
+        {
+            var input = PrepareInput(@"
+1
+00:02:17,440 --> 00:02:20,375
+Senator, we're making
+our final approach into Coruscant.
+
+2
+00:02:20,476 --> 00:02:22,501
+Very good, Lieutenant.");
+
+            var count = WordCounter.Count(input, "approach");
+            count.Should().Be(1);
+        }
+
+        [Test]
+        public void CountAllWordsTest()
+        {
+            using (var sr = new StreamReader(new FileStream("Star.Wars.srt", FileMode.Open, FileAccess.Read)))
+            {
+                var count = WordCounter.AllWordsCount(sr);
+                Console.WriteLine(count);
+            }
+        }
+
+        [Test]
+        public void TimeEstimateTest()
+        {
+            var words = new Dictionary<string, uint>();
+            var sw = new Stopwatch();
+            sw.Start();
+
+            for (int t = 0; t < 100; t++)
+            {
+                using (var sr = new StreamReader(new FileStream("Star.Wars.srt", FileMode.Open, FileAccess.Read)))
+                {
+                    WordCounter.AnalyzeFile(sr, words);
+                }
+            }
+
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+        }
+
+        
+    }
+}
